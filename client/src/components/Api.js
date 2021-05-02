eb43f4084e84e5cdc90ffcf2cb4a7626b68bc11d
ordegani <oradeganii@gmail.com>
2021-05-02 18:49:20 +0300

    change

---
+++ client/src/components/Api.js
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+
+// const API = 'http://www.wikiart.org/en/App/Painting/PaintingsByArtist?artistUrl=${query}&json=2';
+// const API_KEY ='';
